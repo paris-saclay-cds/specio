39f06b857755ab911a4cca080ac7046ab3cd77b5
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-07-26 17:51:49 +0200

    EHN add datasets module and example for SPC

---
+++ examples/plot_read_spc.py
@@ -0,0 +1,29 @@
+"""
+====================================================
+Read SPC Galactic Industries Corporation binary file
+====================================================
+
+This example shows how to read SPC file and plot the results.
+
+"""
+
+from __future__ import print_function
+
+import matplotlib.pyplot as plt
+
+from specio import specread
+from specio.datasets import load_spc_path
+
+# Find the path to the SPC toy data
+spc_filename = load_spc_path()
+print(spc_filename)
+
+# Read the data
+spectra = specread(spc_filename)
+
+# Plot the first spectra
+plt.plot(spectra.wavelength,
+         spectra.spectrum[0])
+plt.xlabel(spectra.meta['xlabel'])
+plt.ylabel(spectra.meta['ylabel'])
+plt.show()
---
+++ specio/datasets/__init__.py
@@ -0,0 +1,9 @@
+"""
+The module `specio.datasets` includes some toy file which are supported by
+the plugins reader.
+"""
+
+from .spc import load_spc_path
+
+
+__all__ = ['load_spc_path']
