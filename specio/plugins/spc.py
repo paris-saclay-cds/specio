43d700e62a2fc9b7a9436700a25b4c91a7683f92
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-12-07 14:52:03 +0100

    FIX add available attributes in meta data (#14)
    * FIX add available attributes in meta data
    
    * EHN reject underscore attribute in metadata

--- specio/plugins/spc.py
+++ specio/plugins/spc.py
@@ -19,6 +19,7 @@ from ..core.util import Spectrum
 
 
 VERSION_SUPPORTED = (b'\x4b', b'\x4d', b'\xcf')
+IGNORED_ATTRIBUTES = ('x', 'sub')
 
 
 class SPC(Format):
@@ -84,100 +85,9 @@ class SPC(Format):
                 A dictionary with the meta data.
 
             """
-            if spc_file.fversn == b'\x4b':
-                meta = {'ftflg': spc_file.ftflg,
-                        'fversn': spc_file.fversn,
-                        'fexpr': spc_file.fexper,
-                        'fexp': spc_file.fexp,
-                        'fnpts': spc_file.fnpts,
-                        'ffirst': spc_file.ffirst,
-                        'flast': spc_file.flast,
-                        'fnsuv': spc_file.fnsub,
-                        'fxtype': spc_file.fxtype,
-                        'fytype': spc_file.fytype,
-                        'fztype': spc_file.fztype,
-                        'fpost': spc_file.fpost,
-                        'fdate': spc_file.fdate,
-                        'fres': spc_file.fres,
-                        'fsource': spc_file.fsource,
-                        'fpeakpt': spc_file.fpeakpt,
-                        'fspare': spc_file.fspare,
-                        'fcmnt': spc_file.fcmnt,
-                        'fcatxt': spc_file.fcatxt,
-                        'flogoff': spc_file.flogoff,
-                        'fmods': spc_file.fmods,
-                        'fprocs': spc_file.fprocs,
-                        'flevel': spc_file.flevel,
-                        'fsamplin': spc_file.fsampin,
-                        'ffactor': spc_file.ffactor,
-                        'fmethod': spc_file.fmethod,
-                        'fzinc': spc_file.fzinc,
-                        'fwplanes': spc_file.fwplanes,
-                        'fwinc': spc_file.fwinc,
-                        'fwtype': spc_file.fwtype,
-                        'freserv': spc_file.freserv,
-                        'tsprec': spc_file.tsprec,
-                        'tcgram': spc_file.tcgram,
-                        'tmulti': spc_file.tmulti,
-                        'trandm': spc_file.trandm,
-                        'tordrd': spc_file.tordrd,
-                        'talabs': spc_file.talabs,
-                        'txyxys': spc_file.txyxys,
-                        'txvals': spc_file.txvals,
-                        'year': spc_file.year,
-                        'month': spc_file.month,
-                        'day': spc_file.day,
-                        'hour': spc_file.hour,
-                        'minute': spc_file.minute,
-                        'cmnt': spc_file.cmnt,
-                        'dat_fmt': spc_file.dat_fmt,
-                        'logsizd': spc_file.logsizd,
-                        'logsizm': spc_file.logsizm,
-                        'logtxto': spc_file.logtxto,
-                        'logbins': spc_file.logbins,
-                        'logdsks': spc_file.logdsks,
-                        'logspar': spc_file.logspar,
-                        'log_dict': spc_file.log_dict,
-                        'spacing': spc_file.spacing,
-                        'xlabel': spc_file.xlabel,
-                        'ylabel': spc_file.ylabel,
-                        'zlabel': spc_file.zlabel,
-                        'exp_type': spc_file.exp_type}
-            elif spc_file.fversn == b'\x4d':
-                meta = {'oftflgs': spc_file.oftflgs,
-                        'oversn': spc_file.oversn,
-                        'oexp': spc_file.oexp,
-                        'onpts': spc_file.onpts,
-                        'ofirst': spc_file.ofirst,
-                        'olast': spc_file.olast,
-                        'fxtype': spc_file.fxtype,
-                        'fytype': spc_file.fytype,
-                        'oyear': spc_file.oyear,
-                        'omonth': spc_file.omonth,
-                        'oday': spc_file.oday,
-                        'ohour': spc_file.ohour,
-                        'ominute': spc_file.ominute,
-                        'ores': spc_file.ores,
-                        'opeakpt': spc_file.opeakpt,
-                        'onscans': spc_file.onscans,
-                        'ospare': spc_file.ospare,
-                        'ocmnt': spc_file.ocmnt,
-                        'ocatxt': spc_file.ocatxt,
-                        'osubh1': spc_file.osubh1,
-                        'tsprec': spc_file.tsprec,
-                        'tcgram': spc_file.tcgram,
-                        'tmulti': spc_file.tmulti,
-                        'trandm': spc_file.trandm,
-                        'tordrd': spc_file.tordrd,
-                        'talabs': spc_file.talabs,
-                        'txyxys': spc_file.txyxys,
-                        'txvals': spc_file.txvals,
-                        'fztype': spc_file.fztype,
-                        'xlabel': spc_file.xlabel,
-                        'ylabel': spc_file.ylabel,
-                        'zlabel': spc_file.zlabel}
-            else:
-                meta = {}
+            meta = {key: val
+                    for key, val in spc_file.__dict__.items()
+                    if key not in IGNORED_ATTRIBUTES or key.startswith('_')}
 
             return meta
 
