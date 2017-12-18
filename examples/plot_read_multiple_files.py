08f241a21100a058d001f4eb55af55b3e6107081
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-12-18 12:10:44 +0100

    DOC fix relative path to data

--- examples/plot_read_multiple_files.py
+++ examples/plot_read_multiple_files.py
@@ -10,13 +10,14 @@ folder.
 from __future__ import print_function
 
 import os
+import sys
 
 import matplotlib.pyplot as plt
 
 from specio import specread
 
 # Get the path to the data relatively to this example
-DATA_PATH = os.path.dirname(__file__)
+DATA_PATH = os.path.dirname(sys.argv[0])
 
 spc_filenames = os.path.join(DATA_PATH, 'data', '*.spc')
 print('The SPC files will be search in: {}'.format(spc_filenames))
