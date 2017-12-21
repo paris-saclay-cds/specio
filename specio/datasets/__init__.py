e15dbba0245f04561952464e3e5f7d5dd2216cd2
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-12-21 20:37:20 +0100

    MAINT merge the different datasets (#38)

--- specio/datasets/__init__.py
+++ specio/datasets/__init__.py
@@ -3,9 +3,9 @@ The module `specio.datasets` includes some toy file which are supported by
 the plugins reader.
 """
 
-from .fsm import load_fsm_path
-from .sp import load_sp_path
-from .spc import load_spc_path
+from .path import load_fsm_path
+from .path import load_sp_path
+from .path import load_spc_path
 
 
 __all__ = ['load_fsm_path',
