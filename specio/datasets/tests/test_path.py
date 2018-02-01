d4e8e61bc6096497d6653ee779ace6680d743643
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-02-01 23:40:33 +0100

    EHN add CSV reader

--- specio/datasets/tests/test_path.py
+++ specio/datasets/tests/test_path.py
@@ -12,6 +12,7 @@ from specio.datasets import load_fsm_path
 from specio.datasets import load_mzml_path
 from specio.datasets import load_sp_path
 from specio.datasets import load_spc_path
+from specio.datasets import load_csv_path
 
 
 @pytest.mark.parametrize(
@@ -19,6 +20,7 @@ from specio.datasets import load_spc_path
     [(load_fsm_path(), 'fsm'),
      (load_mzml_path(), 'mzml'),
      (load_sp_path(), 'sp'),
-     (load_spc_path(), 'spc')])
+     (load_spc_path(), 'spc'),
+     (load_csv_path(), 'csv')])
 def test_load_spectra_path(path_data, extension):
     assert os.path.join('datasets', 'data', 'spectra.' + extension) in path_data
