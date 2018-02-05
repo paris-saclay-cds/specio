1694fede9bef89c72013ec655863f52c5ec33511
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-02-05 21:13:40 +0100

    Update: convert subcommand + actually do conversion to csv

--- specio/cli.py
+++ specio/cli.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+import os
+
 from . import specread
 
 
@@ -7,18 +9,31 @@ def main():
     import argparse
     parser = argparse.ArgumentParser(
         prog='specio',
-        description='Convert spectroscopic files.')
+        description='Python input/output for spectroscopic files')
 
-    parser.add_argument(
+    subparsers = parser.add_subparsers(metavar='', dest='sub')
+
+    convert_parser = subparsers.add_parser(
+        'convert',
+        description='Convert spectroscopic files.',
+        help='Convert spectroscopic files.')
+
+    convert_parser.add_argument(
         'filepath', help='The file to convert.')
 
-    parser.add_argument(
+    convert_parser.add_argument(
         '--output', '-o', nargs=1,
         help='The output file path. If not specified, use same path and name '
              'as input with different extension.')
 
     args = parser.parse_args()
 
-    spectrum = specread(args.filepath)
-    print(spectrum)
-    print(args.output)
+    if args.sub == 'convert':
+        spectrum = specread(args.filepath)
+        if args.output:
+            output_path = args.output[0]
+        else:
+            output_path = os.path.splitext(args.filepath)[0] + '.csv'
+
+        spectrum.to_csv(output_path)
+        print("Written {}".format(output_path))
